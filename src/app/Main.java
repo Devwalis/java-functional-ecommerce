5511e11a657e1856f7935d87e9d5999b05671b4b
Walisson <Walissonoliveira1@outlook.com>
2026-06-10 17:36:21 -0300

    Feat: implement main menu

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,16 +1,26 @@
 package app;
 
+
+import menu.Menu;
+
+
 public class Main {
 	
 
 
 	public static void main(String[] args){
 		printWelcomeMessage();
+
+		Menu.show();
+		
 		}	
 
 
 	public static void printWelcomeMessage(){
-		System.out.println("Welcome");
+
+		System.out.println();
+		System.out.println("Welcome to Java Functional Ecommerce");
+		System.out.println();
 	}
 		
-	}
+			}
