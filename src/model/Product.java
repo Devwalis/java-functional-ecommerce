de8017cb8b43d5d3c732809fc1b3ed3753ae3bcd
Walisson <Walissonoliveira1@outlook.com>
2026-06-10 19:33:00 -0300

    Product class encapsulation and constructor

---
+++ src/model/Product.java
@@ -0,0 +1,26 @@
+package model;
+
+
+	public class Product{
+
+		private Long id;
+		
+		private String name;
+		
+		private Double price;
+		
+		private Integer stock;
+		
+
+		public Product(
+		Long id,
+		String name,
+		Double price,
+		Integer stock		
+	
+)
+		
+		}
+
+
+	
