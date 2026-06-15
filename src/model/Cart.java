20458b2f422877e0c31da000bfa1e3d6317a0d9c
Walisson <Walissonoliveira1@outlook.com>
2026-06-15 09:22:55 -0300

    Construction of the shopping cart class

---
+++ src/model/Cart.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart{
+		
+		private List<Product> products;
+	
+
+		public Cart(){
+		
+		this.products = ArrayList<>();
+	
+	}
+		public List<Product> getProduct(){
+			return products;
+		
+	}
+		public void setProducts(List<Product> products){
+			this.products = products;
+	
+		}
+	}
